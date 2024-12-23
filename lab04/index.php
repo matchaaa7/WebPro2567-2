b5e2da215a6aca43a578af06116033411607a774
matchaaa7 <rayzera000@gmail.com>
2024-12-23 15:18:53 +0700

    Lab04 : Classroom Table

---
+++ lab04/index.php
@@ -0,0 +1,108 @@
+<?php
+$classRoomData = [
+        "Mon" => [
+        13 => "Data Analytic",
+        14 => "Data Analytic",
+        15 => "Data Analytic",
+        16 => "Data Analytic",
+        ], 
+        "Tue" => [
+            13=>"Work Integrated Knowledge Sharing",
+            14=>"Work Integrated Knowledge Sharing",
+            15=>"Work Integrated Knowledge Sharing",
+            16=>"Work Integrated Knowledge Sharing",
+        ],
+        "Wed" =>[
+            9 => "Enterpreneurship Knowledge Sharing",
+            10 => "Enterpreneurship Knowledge Sharing",
+            11 => "Enterpreneurship Knowledge Sharing",
+        ],
+        "Thu" => [],
+
+        "Fri" => [
+            8 => "Artificial Intelligence",
+            9 => "Artificial Intelligence",
+            10 => "Artificial Intelligence",
+            11 => "Artificial Intelligence",
+        ],
+
+        "Sat" => [],
+        "Sun" => []
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        table,
+        th,
+        td {
+            width: 50%;
+            border: white 2px solid;
+            border-collapse: collapse;
+            min-width: 90px;
+            
+        }
+
+        #classroom thead {
+            background-color: #0f0f0f ;
+            
+        }
+
+        #classroom
+        {
+            background-color: lightgray;
+        }
+        
+        #classroom th{
+            color: white;
+        }
+
+        #classroom tbody th{
+            background-color: gray;
+            color: white;
+        }
+        .holiday{
+            background-color: crimson !important;
+        
+        }
+    </style>
+</head>
+<body>
+
+    <table id="classroom">
+        <thead>
+            <tr>
+                <th>Day/Time</th>
+                <th>8:00-9:00</th>
+                <th>9:00-10:00</th>
+                <th>10:00-11:00</th>
+                <th>11:00-12:00</th>
+                <th>12:00-13:00</th>
+                <th>13:00-14:00</th>
+                <th>14:00-15:00</th>
+                <th>15:00-16:00</th>
+                <th>16:00-17:00</th>
+        
+            </tr>
+        </thead>
+    <tbody>
+        <?php foreach ($classRoomData as $day => $subject) { ?>
+        <tr>
+            <th> <?php echo $day ?> </th>
+            <?php for($i=8;$i<17;$i++){ ?>
+            <td>
+                <?php echo !empty($subject[$i]) ? $subject[$i] : "" ?>
+            </td>
+            <?php } ?>
+            </tr>
+            <?php } ?>
+    </tbody>
+            </table>
+</body>
+</html>
